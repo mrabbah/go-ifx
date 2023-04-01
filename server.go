e3ed47b8f772de237062eaecf04f67711ca69082
myassine <myassine@rabbahsoft.ma>
2023-04-01 00:05:45 +0000

    test linux

--- server.go
+++ server.go
@@ -2,7 +2,6 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  *---------------------------------------------------------------------------------------*/
-
 package main
 
 import (
@@ -17,7 +16,7 @@ func handle(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, hello.Hello())
 }
 
-func main() {
+func main2() {
 	portNumber := "9000"
 	http.HandleFunc("/", handle)
 	fmt.Println("Server listening on port ", portNumber)
